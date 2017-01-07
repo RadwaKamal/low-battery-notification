571809a601fb9f8740105cb5cb832edf3f207c52
RadwaKamal <clumsyshark@gmail.com>
2017-01-07 16:50:14 +0200

    refactor notify function variables names

--- low_battery.sh
+++ low_battery.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function notify {
-    battery_status=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 \
+    battery_percentage=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 \
                     | grep 'percentage'`
-    percent=`echo "${battery_status//[!0-9]}"`
-    if [ "$percent" -lt "$1" ] 
+    percentage=`echo "${battery_percentage//[!0-9]}"`
+    if [ "$percentage" -lt "$1" ] 
         then
             notify-send -u critical \
             -i notification-battery-low \
-            "Battery is low" "only $percent% left"
+            "Battery is low" "only $percentage% left"
             sleep 1000s
     fi
 }
